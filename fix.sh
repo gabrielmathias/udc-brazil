036ba58780ecddc56a728dd03ab5f4724d13c6ea
Gabriel <gabriel@monolith>
2017-07-20 06:09:53 -0300

    Adding error check

--- fix.sh
+++ fix.sh
@@ -13,12 +13,18 @@ sed -E $"s/^([	 ]+[0-9][^a-zA-Z 	]+)/\<06\>\1/" $arquivo | \
 
 # Remove as entradas de EXEMPLOS
 grep -v "Exemplo(" | \
+sed -E "s/(.*)[	]+(.*)/\1 \2/g"  | \
 
-# Tudo que comecar por tab eh exemplo <09>
-sed -E $"s/^([	 ]*-\>[	 ]*)(.+)$/\<09\>\2/" | \
+# Tudo que comecar por tab + seta eh exemplo <09>
+#sed -E $"s/->([ 	]*)(.+)$/\<09\>\2/" | \
+sed -E "s/->[ ](.+)$/\<09\> \1/" | \
 
-sed -E $"s/^([0-9][^a-zA-Z 	]+)(.*)/\\$ENTER\<01\>\1\\$ENTER\<02\>\2/g" | \
-sed -E $"s/(\<..\>)[	 ]+(.*)$/\1\2/g"  | \
+sed -E $"s/^([0-9][^a-zA-Z 	]+)[ 	]*(.*)/\\$ENTER\<01\>\1\\$ENTER\<02\>\2/g" | \
+sed -E $"s/(\<[0-9][0-9]\>)[]?[	]?[ ]?(.*)$/\1\2/g"  | \
+sed -E "s// /g"  | \
+sed -E $"s/^[ ]+(.*)/\1/"  | \
+sed -E "s/>[ ]+/>/g"  | \
+sed -E "s/(<02>l.*)$/#ERRO \1/" | \
 
 tee $saida | \
 
